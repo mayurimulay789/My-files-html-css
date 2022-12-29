cb76f841d8aff20123439279268516f14d4b0981
mayurimulay789 <121185520+mayurimulay789@users.noreply.github.com>
2022-12-29 17:49:47 +0530

    added arrow function

--- arrowfunction.js
+++ arrowfunction.js
@@ -6,5 +6,9 @@
 //one lineers do not reauired return braces/return
 //one line will automatically return
 //const green = ()=> "Good Night"
-const green = name=> 'good night' +  name ; 
-console.log(green ( 'Mayuri' ));
+// single parameter do not need parathesis to put parathanthesis if multiple parameter
+//const green = name=> 'good night' +  name ; 
+//console.log(green ( 'Mayuri' ));
+const car = (Audi,volvo) => 'this is my cars';
+console.log(car);
+
