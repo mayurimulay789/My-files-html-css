19f6acde52e36367e3e03fa9b9987d761061f004
mayurimulay789 <121185520+mayurimulay789@users.noreply.github.com>
2022-12-29 17:20:26 +0530

    added more function of arrow

---
+++ arrowfunction.js
@@ -0,0 +1,10 @@
+
+// const harry = ()=>{
+// console.log("good morning");
+// }
+// harry();
+//one lineers do not reauired return braces/return
+//one line will automatically return
+//const green = ()=> "Good Night"
+const green = name=> 'good night' +  name ; 
+console.log(green ( 'Mayuri' ));
